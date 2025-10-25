55fe46dc586ebb8e901d2de9e52166fa3b240141
Guiltybench <alanchiquete9780@gmail.com>
2025-10-25 01:17:44 -0700

    Update battle-animations.ts

--- play.pokemonshowdown.com/src/battle-animations.ts
+++ play.pokemonshowdown.com/src/battle-animations.ts
@@ -1880,7 +1880,7 @@ export class PokemonSprite extends Sprite {
 		mudsport: ['Mud Sport', 'good'],
 		substitute: null,
 		divination: null,
-		sacredbody: ['Blessed', 'good'],
+		sacredbody: ['Sacred Body', 'good'],
 		// sub graphics are handled elsewhere, see Battle.Sprite.animSub()
 		uproar: ['Uproar', 'neutral'],
 		rage: ['Rage', 'neutral'],
