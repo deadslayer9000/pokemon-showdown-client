d785a5216c26231aec873e1af20f5c7de42ecc5f
Guiltybench <alanchiquete9780@gmail.com>
2026-08-05 21:35:30 -0700

    Update battle-dex-search.ts

--- play.pokemonshowdown.com/src/battle-dex-search.ts
+++ play.pokemonshowdown.com/src/battle-dex-search.ts
@@ -736,6 +736,7 @@ abstract class BattleTypedSearch<T extends SearchType> {
 			this.dex = Dex.mod('ghostingpatch' as ID);
 		}
 		if (format.includes('ddls4') || format.includes('deltadraftleagueseason4')) {
+			format = format.slice(0 ,-4) as ID;
 			this.formatType = 'ddls4';
 			this.dex = Dex.mod('ddls4patch' as ID);
 		}
